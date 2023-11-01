0f0a64f23ea5a408dc1a4b2bf7bc7bf8654408d6
Jonas Byström <jonas.bystrom@consultant.volvo.com>
2023-11-01 12:29:26 +0100

    Fixed parsing of new PDF format

--- bcp47/generate-bcp47.py
+++ bcp47/generate-bcp47.py
@@ -1,58 +1,101 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
-from PyPDF2 import PdfFileReader
+from PyPDF2 import PdfReader
 import os
 import requests
 
 
 url = 'https://winprotocoldoc.blob.core.windows.net/productionwindowsarchives/MS-LCID/[MS-LCID].pdf'
+language_markers = [' Windows ', ' Release ']
+bad_markers = ['release:', 'operating', 'server', 'first', 'supported']
+join_markers = ['Pseudo', 'Standard']
+languages = set()
+countries = set()
 
 
-def extract_phrases(i, step, phrases):
-    ps = []
-    while i > 0 and i < len(phrases):
-        p = phrases[i].strip()
-        pl = p.lower()
-        if 'version' in pl or 'release' in pl or 'windows' in pl:
-            if step < 0 and pl in ('release', 'windows'): # uh-oh, previous word was version of release
-                ps = ps[:-1]
-            break
-        if p:
-            ps.append(p)
-        i += step
-    if step < 0:
-        ps = reversed(ps)
-    return ps
+def is_language_line(phrase):
+    return any(lm in phrase for lm in language_markers) and not any(bm in phrase.lower() for bm in bad_markers)
+
+
+def cleanup(word):
+    if word:
+        word = word.replace(' -', '-')
+        if word.startswith('('):
+            word = word.replace('(','').replace(')','')
+    return word
+
+
+def extract_phrase_name_tag(phrase):
+    data = phrase
+    for lm in language_markers:
+        data = data.split(lm)[0].strip()
+    data = data.replace('Latin', '(Latin)').replace('((','(').replace('))',')').replace('),', ') ')
+    es = [w.strip() for w in data.split('  ') if w.strip()]
+    words = []
+    for e in es:
+        if ' 0x' in e:
+            words += e.split()
+        else:
+            words += [e]
+    if len(words) >= 3 and words[1].startswith('('): # ['Javanese', '(Latin)', ...
+        words = [' '.join(words[:2])] + words[2:]
+    if len(words) == 3: # "Zulu South Africa  0x0435  zu-ZA"
+        if words[0].split()[0] in languages and not words[0].endswith(')'):
+            l,_,c = words[0].partition(' ')
+            words = [l,c] + words[1:]
+        if 'City' in words[0] and len(words[0].split()) == 3: # "Latin Vatican City"
+            words = words[0].split(maxsplit=1) + words[1:]
+    if len(words) == 5: # "Spanish  United  States  0x540A  es-US"
+        if words[0] in languages:
+            words = words[:1] + [' '.join(words[1:3])] + words[3:]
+        elif ' '.join(words[:2]) in languages:
+            words = [' '.join(words[:2])] + words[2:]
+    if len(words) == 1: # "MA Release 10"
+        lang,country,tag = None, None, None
+    elif len(words) == 3 and words[1].startswith('0x'):
+        lang,country,tag = words[0], None, words[2]
+    elif len(words) == 4 and words[2].startswith('0x'):
+        lang = words[0]
+        country = words[1].replace(' -', '-')
+        tag = words[3]
+    else:
+        assert False, f'unknown phrase "{phrase}", {words}'
+    lang,country = cleanup(lang),cleanup(country)
+    if tag:
+        tag = tag.replace(' ', '')
+    # print(f'"{phrase}", {words}') # debug
+    languages.add(lang)
+    countries.add(country)
+    name = f'{lang} - {country}' if country else lang
+    return name,tag
 
 
 def generate_page(phrases):
     name_tag = []
     for i,phrase in enumerate(phrases):
-        if phrase.startswith('0x'):
-            names = extract_phrases(i-1, -1, phrases)
-            tags = extract_phrases(i+1, +1, phrases)
-            name = ' - '.join(names)
-            name = name.replace(' - (', ' (') # proper parenthesis
-            name = name.replace(' - - - ', '-') # dash
-            tag = ''.join(tags)
-            name_tag.append((name, tag))
+        # print(i, phrase) # debug
+        if is_language_line(phrase):
+            name,tag = extract_phrase_name_tag(phrase)
+            if name and tag:
+                name_tag.append((name, tag))
             print('.', flush=True, end='')
     return name_tag
 
 
 def join_phrases(phrases):
-    '''PyDFP2 generator output separates cells with a single space line, but none for word split.
-       This joins phrases only separated by a single linefeed.'''
+    '''Join lines when possibly appropriate...'''
     complete_phrases = []
-    last_ps = ''
+    prev = ''
     for phrase in phrases:
-        ps = phrase.strip()
-        if ps and last_ps:
-            complete_phrases[len(complete_phrases)-1] += ps
-        elif ps:
+        phrase = phrase.strip()
+        if phrase.startswith('(') or phrase.partition(' ')[0].endswith(')') \
+                or (len(phrase.split()) <= 4 and is_language_line(phrase)) \
+                or (is_language_line(phrase) and not is_language_line(prev) and any(jm in prev for jm in join_markers)):
+            complete_phrases[-1] += ' ' + phrase
+        else:
             complete_phrases.append(phrase)
-        last_ps = ps
+        prev = phrase
     return complete_phrases
 
 
@@ -66,7 +109,7 @@ def write_output(name_tag):
         for name,tag in name_tag:
             if name in namecache:
                 name += ', ' + tag.split('-')[-1]
-            assert name not in namecache
+            assert name not in namecache, f'{name} already entered before'
             namecache.add(name)
             print('    "%s": "%s",' % (name, tag), file=w)
         print('}', file=w)
@@ -85,11 +128,9 @@ if not os.path.exists('.buffer.pdf'):
 # open locally stored file and parse that
 name_tag = [] # keeps name+tag tuples
 with open('.buffer.pdf', 'rb') as r:
-    reader = PdfFileReader(r)
+    reader = PdfReader(r)
     for page in reader.pages:
-        phrases = page.extractText().splitlines()
-        if 'Language' not in phrases:
-            continue
+        phrases = page.extract_text().splitlines()
         phrases = join_phrases(phrases)
         name_tag += generate_page(phrases)
 write_output(name_tag)
