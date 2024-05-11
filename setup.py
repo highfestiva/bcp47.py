5f841fc99c994b9c5d9f7fb986b07b7646205383
Jonte <highfestiva@gmail.com>
2024-05-11 18:09:33 +0200

    Release v0.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='bcp47',
-    version='0.0.4',
+    version='0.0.5',
     author='Jonas Byström',
     author_email='highfestiva@gmail.com',
     description='Language tags made easy',
